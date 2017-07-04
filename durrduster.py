736ade2fa2010cf7c8899404b3fc85e937c0fad0
Hugh Nowlan <nosmo@nosmo.me>
2017-07-04 20:36:12 +0100

    Add futures support, concurrency

--- durrduster.py
+++ durrduster.py
@@ -3,6 +3,8 @@
 import copy
 import random
 
+from concurrent.futures import ThreadPoolExecutor
+from requests_futures.sessions import FuturesSession
 import requests
 import yaml
 
@@ -12,6 +14,7 @@ from wordlist import Wordlist
 FOLLOW_REDIRECTS = False
 MAX_DEPTH = 3
 DEFAULT_USER_AGENT = "Mozilla/5.0 (Macintosh; MacOS 7.5.1) You should probably block this UA"
+MAX_CONCURRENT = 10
 
 # Session makes use of auth (etc) easier, but also makes detection
 # easier if cookies are set
@@ -25,6 +28,7 @@ CONFIG_DICT = {
         "filename": ["filename.txt"]
     },
     "user_agents": ["useragents.txt"],
+    "max_concurrent": MAX_CONCURRENT,
 }
 
 
@@ -39,7 +43,7 @@ class Durrduster(object):
         self.user_agents = []
 
         if USE_SESSION:
-            self.session = requests.Session()
+            self.session = FuturesSession(executor=ThreadPoolExecutor(max_workers=MAX_CONCURRENT))
             self.methods = make_session_methods(self.session)
         else:
             self.methods = HTTP_METHODS
@@ -57,57 +61,93 @@ class Durrduster(object):
                     self.user_agents.append(useragent)
 
     def brute_section(self, brute_iterator, method, follow_redirects, prefix=None):
+        brute_futures = []
+
         # Brute_iterator could be a list of paths, filenames, mutated paths
         for component in brute_iterator:
             request_path = component
             if prefix and prefix != "/":
                 request_path = "%s/%s" % (prefix, component)
 
-            print("request path is %s" % request_path)
-            request_obj = self.check(request_path, method, follow_redirects)
-            if request_obj.ok:
-                print("Hit for %s" % request_obj.url)
-                print(request_obj.status_code)
+            #print("request path is %s" % request_path)
+            future_obj = self.check(request_path, method, follow_redirects)
+            #if request_obj.ok:
+            #    print("Hit for %s" % request_obj.url)
+            #    print(request_obj.status_code)
+            brute_futures.append(future_obj)
+        return brute_futures
 
     def brute_dirs(self, dirlist, method, follow_redirects, prefix=None):
-        dir_hits = []
+        dir_futures = []
         for check_dir in dirlist:
             check_path = check_dir
             if prefix and prefix != "/":
                 check_path = "/".join([prefix, check_dir])
 
             request_obj = self.check(check_path, method, follow_redirects)
-            if request_obj.ok:
-                print("Hit for %s" % request_obj.url)
-                print(request_obj.status_code)
-                dir_hits.append(check_path)
-        return dir_hits
+            #if request_obj.ok:
+            #    print("Hit for %s" % request_obj.url)
+            #    print(request_obj.status_code)
+            #    dir_hits.append(check_path)
+            dir_futures.append(request_obj)
+        return dir_futures
 
 
     def brute(self, follow_redirects, max_depth, method="GET"):
 
+        """TODO option to make max_depth be obeyed relative to the last
+        successful dir?
+
+        ie: with max_depth 3, example.com/fail/fail/fail fails out but
+        once we hit example.com/fail/success/, keep going until we hit
+        example.com/fail/success/fail/fail/fail/. Currently we only
+        allow an ABSOLUTE max depth of 3
+
+        """
+
         complete_filelist = self.wordlist.permute_filenames()
 
-        dir_list = self.brute_dirs(
-            self.wordlist.path() + ["/"], method, follow_redirects)
+        dir_futures = self.brute_dirs(
+            self.wordlist.path() + ["/"], method, follow_redirects
+        )
+
+        dir_list = []
+        for dir_future in dir_futures:
+            dir_request = dir_future.result()
+            if dir_request.ok:
+                print("Hit for %s" % dir_request.url)
+                path = dir_request.url.partition(self.domain)[2]
+                dir_list.append(path)
+
         depth = 1
         found_dirs = copy.copy(dir_list)
 
+
+        dir_list = []
         while dir_list and depth != max_depth:
-            for prefix_dir in dir_list:
-                print("Attempting %s" %  prefix_dir)
-                dir_list = self.brute_dirs(
-                    self.wordlist.path(), method, follow_redirects, prefix=prefix_dir
-                )
-                found_dirs += dir_list
-            depth +=1
+            dir_futures = self.brute_dirs(
+                self.wordlist.path(), method, follow_redirects, prefix=prefix_dir
+            )
+            for dir_future in dir_futures:
+                dir_request = dir_future.result()
+                if dir_request.ok:
+                    path = dir_request.url.partition(self.domain)[2]
+                    print("Hit for %s" % dir_request.url)
+                    dir_list.append(path)
+            found_dirs += dir_list
+            depth += 1
         found_dirs = list(set(found_dirs))
 
         print("Finished scanning directories. Dirlist is %s" % str(found_dirs))
 
+        brute_futures = []
         for found_dir in found_dirs:
-            self.brute_section(complete_filelist, method, follow_redirects, prefix=found_dir)
-
+            brute_futures += self.brute_section(
+                complete_filelist, method, follow_redirects, prefix=found_dir)
+        for brute_future in brute_futures:
+            brute_result = brute_future.result()
+            if brute_result.ok:
+                print("Hit for %s" % brute_result.url)
 
     def check(self, component, method, follow_redirects=False):
         #TODO don't follow redirects
@@ -115,6 +155,7 @@ class Durrduster(object):
         headers = requests.utils.default_headers()
         user_agent = random.choice(self.user_agents if self.user_agents else [DEFAULT_USER_AGENT])
         print("User agent is %s" % user_agent)
+        print("Path is %s" % component)
 
         ua_header = {"User-Agent": user_agent}
         headers.update(ua_header)
@@ -132,7 +173,7 @@ class Durrduster(object):
         return get_result
 
     def get_results(self, output_format="human"):
-        raise NotImplemented
+        raise NotImplementedError
 
 def main(config):
 
